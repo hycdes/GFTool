245f351148225442131ff5a4ec75d75cb13b437b
hycdes <hycdes@qq.com>
2019-03-22 14:07:14 +0800

    updt falcon-8s spc-blt get

---
+++ js/language_ChipCal-EN.js
@@ -0,0 +1,42 @@
+lib_lang = {
+  sele_selenum: 'Select number',
+  sele_selebest: 'Best combinations',
+  sele_noresult: 'No combination',
+
+  check_M2: 'Enable 1~3 blank blocks (without 5-block-type-2)',
+
+  btn_clear: 'Press again to clear repository',
+  btn_maxall: 'Press again to max all',
+  btn_blueall: 'Press again to keep only BLUE chips',
+  btn_orangeall: 'Press again to keep only ORANGE chips',
+  btn_savedone: 'Savecode has been copied to the clipboard',
+  btn_loaderror: 'Invalid savecode',
+  btn_showblock: 'Show blocknum',
+  btn_showvalue: 'Show properties',
+
+  over_dmg: 'DMG overflow',
+  over_dbk: 'DBK overflow',
+  over_acu: 'ACU overflow',
+  over_fil: 'FIL overflow',
+
+  blo_5: '5-block',
+  blo_6: '6-block',
+  shape: 'shape',
+  num: 'Num',
+  topo: 'Topology',
+  toponum: 'Topology num',
+  notopo: 'No solution',
+  totaltime: 'Time consumption',
+  refer: 'Sorting by',
+  valid_value: 'total valid properties',
+  valid_block: 'total valid blocknum',
+  dmg: 'DMG',
+  dbk: 'DBK',
+  acu: 'ACU',
+  fil: 'FIL',
+  sorting: ' ',
+  have: 'here are',
+  type_of_combi: 'valid combinations',
+  showall: ' [Show all] output each combination of each topology (few seconds)',
+  showsort: ' [Sort all] output specified number of solutions (few minutes)'
+}
