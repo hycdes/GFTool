ed027b6ed3bd602167c2f8ac5dd81ba7bfa280eb
hycdes <hycdes@qq.com>
2019-05-03 23:01:20 +0800

    updt stat

--- js/Stat_main.js
+++ js/Stat_main.js
@@ -41,11 +41,11 @@ var data_map = {
   m02: [2, 5, true, 5]
 }
 var data_116true = [[4, 'Colt Revolver', 1], [4, 'AS Val', 1], [4, 'SpringField', 1], [4, 'M1918', 1], [4, 'Mk46', 2],
-  [3, 'M9', 3], [3, 'P08', 8], [3, 'Type 92', 5], [3, 'Tokarev', 1],
-  [3, 'OTs-12', 5], [3, 'StG44', 6],
+  [3, 'M9', 3], [3, 'P08', 8], [3, 'Type 92', 5], [3, 'Tokarev', 2],
+  [3, 'OTs-12', 5], [3, 'StG44', 7],
   [3, 'MAC-10', 1], [3, 'PPS-43', 2], [3, 'Sten MkII', 5],
   [3, 'M1 Garand', 7], [3, 'SV-98', 8],
-  [3, 'Bren', 7], [3, 'M1919A4', 5]]
+  [3, 'Bren', 7], [3, 'M1919A4', 6]]
 var data_116false = [[4, 'Mk46', 2],
   [3, 'M9', 2],
   [3, 'OTs-12', 3], [3, 'StG44', 1],
@@ -53,12 +53,12 @@ var data_116false = [[4, 'Mk46', 2],
   [3, 'SV-98', 1], [3, 'M1 Garand', 1]
 ]
 var data_104e5true = [[5, 'SR-3MP', 1],
-  [4, 'Mk23', 1], [4, 'AS Val', 1], [4, 'PP-90', 1], [4, 'XM3', 6], [4, 'M60', 1],
-  [3, 'Astra Revolver', 3], [3, 'C96', 10], [3, 'M9', 1], [3, 'Makarov', 5],
-  [3, 'AK-47', 4], [3, 'FNC', 4],
-  [3, 'MAC-10', 6], [3, 'Micro UZI', 7], [3, 'Skorpion', 6],
-  [3, 'M14', 4],
-  [3, 'M2HB', 5], [3, 'MG42', 3]]
+  [4, 'Mk23', 1], [4, 'AS Val', 2], [4, 'PP-90', 1], [4, 'XM3', 7], [4, 'M60', 1],
+  [3, 'Astra Revolver', 6], [3, 'C96', 15], [3, 'M9', 1], [3, 'Makarov', 7],
+  [3, 'AK-47', 4], [3, 'FNC', 8],
+  [3, 'MAC-10', 8], [3, 'Micro UZI', 9], [3, 'Skorpion', 10],
+  [3, 'M14', 8],
+  [3, 'M2HB', 8], [3, 'MG42', 3]]
 var data_104e6false = [[5, 'SR-3MP', 1],
   [4, 'AS Val', 1],
   [3, 'Astra Revolver', 2], [3, 'C96', 6], [3, 'M9', 2], [3, 'Makarov', 3],
@@ -97,11 +97,14 @@ get_card('card_104e6_2', data_map.m104e6)
 get_card('card_104e7', data_map.m104e7)
 get_card('card_104e7_2', data_map.m104e7)
 get_card('card_02', data_map.m02)
-fill_table('stat_116true', true, 'table_116true', data_116true, 320)
+
+fill_table('stat_116true', true, 'table_116true', data_116true, 336)
 fill_table('stat_116false', false, 'table_116false', data_116false, 50)
+
 fill_table('stat_115true', true, 'table_115true', data_115true, 50)
 fill_table('stat_115false', false, 'table_115false', data_115false, 415)
-fill_table('stat_104e5true', true, 'table_104e5true', data_104e5true, 335)
+
+fill_table('stat_104e5true', true, 'table_104e5true', data_104e5true, 555)
 fill_table('stat_104e6false', false, 'table_104e6false', data_104e6false, 252)
 fill_table('stat_104e7true', true, 'table_104e7true', data_104e7true, 381)
 fill_table('stat_104e7false', false, 'table_104e7false', data_104e7false, 182)
