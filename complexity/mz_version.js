972d3e8af30450ec9daff9ac917513728019493b
hycdes <hycdes@qq.com>
2022-11-19 14:51:59 +0800

    end-transfer-mz

--- complexity/mz_version.js
+++
@@ -1,9 +0,0 @@
-var _version = {
-  CL: 'v1.0 (2022.11.10)'
-}
-var _updateinfo = {
-  ES: 'mg4 xm3'
-}
-var _navitem = [
-  '<li><a href="missionzero.html"><i class="fa fa-book fa-fw"></i> 玩法工具</a></li>',
-]
